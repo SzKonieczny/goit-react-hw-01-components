6d8f974e9c851b8462ee0b800af4fd4486b8e00d
Szymon Konieczny <szymon_konieczny@o2.pl>
2023-10-17 21:10:28 +0200

    App.jsx; data, friends, transactions, user.json added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
+import user from '../data/user';
+import data from '../data/data';
+import friends from '../data/friends';
+import transactions from '../data/transactions';
+import Profile from '../profile/Profile';
+import Statistics from '../statistics/Statistics';
+import FriendList from '../friendList/FriendList';
+import TransactionHistory from '../transactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    // <div
+    //   style={{
+    //     height: '100vh',
+    //     display: 'flex',
+    //     justifyContent: 'center',
+    //     alignItems: 'center',
+    //     fontSize: 40,
+    //     color: '#010101',
+    //   }}
+    // >
+    //   React homework template
+    // </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
+    </>
   );
 };
